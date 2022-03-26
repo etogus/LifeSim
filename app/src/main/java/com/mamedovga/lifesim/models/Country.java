5ec0743de2d92da6ff1a4288028a29e9ad0938a9
etogus <mamedov-guseyn@mail.ru>
2022-03-26 12:52:45 +0300

    Added toString() to fix country name in the spinner

--- app/src/main/java/com/mamedovga/lifesim/models/Country.java
+++ app/src/main/java/com/mamedovga/lifesim/models/Country.java
@@ -1,5 +1,7 @@
 package com.mamedovga.lifesim.models;
 
+import androidx.annotation.NonNull;
+
 public class Country {
 
     private String name;
@@ -14,5 +16,11 @@ public class Country {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return getName();
     }
 }
