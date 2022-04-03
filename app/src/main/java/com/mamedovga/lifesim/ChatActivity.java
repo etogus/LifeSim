e033eee74fb9bfab1564a6ec111c92c5f202181c
etogus <mamedov-guseyn@mail.ru>
2022-04-03 20:00:15 +0300

    Adding chat functionality

--- app/src/main/java/com/mamedovga/lifesim/ChatActivity.java
+++ app/src/main/java/com/mamedovga/lifesim/ChatActivity.java
@@ -1,16 +1,24 @@
 package com.mamedovga.lifesim;
 
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.mamedovga.lifesim.databinding.ChatBinding;
 import com.mamedovga.lifesim.models.Chat;
+import com.mamedovga.lifesim.models.Response;
 
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ChatActivity extends AppCompatActivity {
     private ChatBinding binding;
@@ -36,12 +44,40 @@ public class ChatActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(binding.editText.getText().toString().isEmpty()) {
                     Toast.makeText(ChatActivity.this, "Введите сообщение", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+                getResp(binding.editText.getText().toString());
+                binding.editText.setText("");
             }
         });
     }
 
     private void getResp(String message) {
+        chatArrayList.add(new Chat(message, USER_KEY));
+        chatAdapter.notifyDataSetChanged();
+        String url = "http://api.brainshop.ai/get?bid=165288&key=kUfecpVDbDxJUmDV&uid=[uid]&msg=" + message;
+        String BASE_URL = "http://api.brainshop.ai/";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class);
+        Call<Response> call = retrofitAPI.getMessage(url);
+        call.enqueue(new Callback<Response>() {
+            @Override
+            public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+                if(response.isSuccessful()) {
+                    Response response1 = response.body();
+                    chatArrayList.add(new Chat(response1.getResponse(), BOT_KEY));
+                    chatAdapter.notifyDataSetChanged();
+                }
+            }
 
+            @Override
+            public void onFailure(Call<Response> call, Throwable t) {
+                chatArrayList.add(new Chat("Повтори сообщение, до меня не дошло", BOT_KEY));
+                chatAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
--- app/src/main/java/com/mamedovga/lifesim/ChatAdapter.java
+++ app/src/main/java/com/mamedovga/lifesim/ChatAdapter.java
@@ -51,6 +51,18 @@ public class ChatAdapter extends RecyclerView.Adapter {
     }
 
     @Override
+    public int getItemViewType(int position) {
+        switch (chatArrayList.get(position).getSender()) {
+            case "user":
+                return 0;
+            case "bot":
+                return 1;
+            default:
+                return -1;
+        }
+    }
+
+    @Override
     public int getItemCount() {
         return chatArrayList.size();
     }
