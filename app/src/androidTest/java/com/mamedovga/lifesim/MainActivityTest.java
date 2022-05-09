8bd0ad0e0680d12e0d803507f07d8dc22b91f747
etogus <mamedov-guseyn@mail.ru>
2022-05-09 22:49:30 +0300

    Added instrumentation test

---
+++ app/src/androidTest/java/com/mamedovga/lifesim/MainActivityTest.java
@@ -0,0 +1,36 @@
+package com.mamedovga.lifesim;
+
+import static org.junit.Assert.*;
+
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    private MainActivity mainActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = activityTestRule.getActivity();
+    }
+
+    @Test
+    public void testLaunch() {
+        View view = mainActivity.findViewById(R.id.start_game_button);
+        assertNotNull(view);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mainActivity = null;
+    }
+}
